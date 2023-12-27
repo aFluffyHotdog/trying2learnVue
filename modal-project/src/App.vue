1eb5160dd26a64adddecd2fbe60ce81a2490a1ba
Mac <macthitiwut@gmail.com>
2023-12-27 15:55:13 +0700

    Used vue props, slots, teleportation woahhhh

---
+++ modal-project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1> {{ title }} </h1>
+  <input type="text" ref="name">
+  <teleport to=".modals" v-if="showModal">
+    <MyModal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now!</a>
+      </template>
+      <h1>This is a title passed in using a slot</h1>
+      <p> and so is this body</p>
+    </MyModal>
+  </teleport>
+  <button @click.alt="toggleModal">Open Modal (+alt)</button>
+</template>
+
+<script>
+import MyModal from './components/MyModal.vue'
+export default {
+  name: 'App',
+  data() {
+    return{
+      title: "My first vue app!",
+      showModal: false
+    }
+  },
+  components: { MyModal },
+  methods:{
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project/src/components/MyModal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class = "modal" :class="{sale: theme === 'sale'}">
+            <slot>Defaul content if nothing's passed in</slot>
+            <div class="action">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['theme'],
+        methods:{
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top:0;
+        position: fixed;
+        background: rgba(0, 0, 0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1 {
+        color: blue;
+        border: none;
+    }
+    
+
+</style>
